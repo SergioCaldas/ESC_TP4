fe4e21eb1bf42bf0b57fa09732849b4954b38a4b
SergioCaldas <sergiocaldas.91@gmail.com>
2016-04-24 20:50:42 +0100

    Added diskHitachi scripts

---
+++ Dtrace/read_hitachi.d
@@ -0,0 +1,38 @@
+#!/usr/sbin/dtrace -s
+
+#pragma D option quiet
+
+syscall::open*:entry
+/execname=="iozone" & uid==29231/
+{
+	self->path = copyinstr(arg1);
+	self->flag = arg2;
+}
+
+syscall::open*:return
+/self->path=="/discHitachi/a57779/iozone.tmp" /
+{
+	self->start_time = timestamp;
+	total_time=0;
+	size = 0;
+	flag = 1;
+	total_time_io = 0;
+}
+
+syscall::read:return
+/self->start_time> 0/
+{
+	self->r_size = (arg0/1024);	
+	size = size + self->r_size;
+}
+
+syscall::close*:entry
+/self->path=="/discHitachi/a57779/iozone.tmp" /
+{
+	self->stop_time = timestamp;
+	self->elapsed = self->stop_time - self->start_time;
+	total_time = total_time + self->elapsed; 
+	printf("%-12s %s\n","SIZE","ELAPSED TIME");
+	printf("%-12d %d\n",size,total_time);
+	flag = 0;
+}
---
+++ Dtrace/write_hitachi.d
@@ -0,0 +1,42 @@
+#!/usr/sbin/dtrace -s
+
+#pragma D option quiet
+
+syscall::open*:entry
+/execname=="iozone" & uid==29231/
+{
+	self->path = copyinstr(arg1);
+}
+
+syscall::open*:return
+/self->path=="/diskHitachi/a57779/iozone.tmp"/
+{
+	total_time=0;
+	size = 0;
+	flag = 1; 
+}
+
+syscall::write:entry
+/flag == 1/
+{
+	self->start_time = timestamp;
+	self->w_size = (arg2/1024);	
+	size = size + self->w_size;
+}
+
+syscall::write:return
+/self->start_time > 0 & self->w_size > 0/
+{
+	self->stop_time = timestamp;
+	self->elapsed = self->stop_time - self->start_time;
+	total_time = total_time + self->elapsed; 
+}
+
+
+syscall::close*:entry
+/self->path=="/diskHitachi/a57779/iozone.tmp"/
+{
+	printf("%-12s %s\n","SIZE","ELAPSED TIME");
+	printf("%-12d %d\n",size,total_time);
+	flag = 0;
+}
